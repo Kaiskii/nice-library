181745ebe78ce149be348767c9520b56dd3c286e
Mygnol <lymprivateacc@gmail.com>
2021-09-11 05:22:06 +0800

    fix: Rearranged file format feat: Imported LONGlib ResourceIndex

---
+++ Assets/Scripts/Long/LongLIB/ResourceIndex/Example Scripts/ExampleIndexFetch.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+An example utilization for the Resource Index.
+*/
+
+public class ExampleIndexFetch : MonoBehaviour
+{
+    void Start()
+    {
+        //GET ALL ASSETS
+        /*
+        List<ExampleResourceSO> allResources = new List<ExampleResourceSO>();
+        allResources.AddRange(ResourceIndex.GetAllAssets<ExampleResourceSO>());
+
+        Debug.Log(allResources.Count);
+        foreach (ExampleResourceSO resource in allResources)
+        {
+            Debug.Log(resource.data);
+        }
+        */
+
+        //GET ASSET BY ID
+        var resourceByID = ResourceIndex.GetAsset<ExampleResourceSO>(0);
+        if(resourceByID) Debug.Log(resourceByID.data);
+    }
+}
